7de2ad91aee92efe3674a7f470349b5b9b82939a
张凯涵 <zane@zaneMacBook-Pro.local>
2025-04-01 12:21:43 +0800

    张凯涵第七次提交：refactoring

--- lists/tests.py
+++ lists/tests.py
@@ -2,10 +2,13 @@ from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
+from django.template.loader import render_to_string
+
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
