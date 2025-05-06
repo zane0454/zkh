675ce411d7325f3d489a8fbaf7fd59592d9ecffb
张凯涵 <zane@zaneMacBook-Pro.local>
2025-05-06 11:41:32 +0800

    张凯涵 message 15：new URL,view and template to display lists

--- notes/urls.py
+++ notes/urls.py
@@ -19,4 +19,5 @@ from django.urls import path
 from lists import views  # 添加这行到文件顶部
 urlpatterns = [
     path('', views.home_page, name='home'),
+    path('lists/the-new-page/', views.view_list, name='view_list'),
 ]
