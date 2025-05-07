900c89b541a8587bc2174fb5874f9596363f1280
张凯涵 <zane@zaneMacBook-Pro.local>
2025-05-07 12:08:08 +0800

    张凯涵 message20:testing for layout + styleing

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -109,5 +109,16 @@ class NewVisitorTest(LiveServerTestCase):
 
         # 两人都很满意，然后去睡觉了
 
+    def test_layout_and_styling(self):
+        # 张三去首页
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
 
+        # 他注意到输入框完美的居中显示
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
 
