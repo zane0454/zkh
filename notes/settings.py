2dacb0c0614f0abe932914990e09d6420ccb5c01
张凯涵 <zane@zaneMacBook-Pro.local>
2025-05-18 22:37:26 +0800

    遗漏部分2

--- notes/settings.py
+++ notes/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-+@brdk=c)g9r3kg@gop-vz-%at&&%p1_jn!#j1503&hvvc+2$h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['47.95.159.4']
-
+ALLOWED_HOSTS = ['47.95.159.4','localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['http://47.95.159.4', 'https://47.95.159.4']
 
 # Application definition
 
